67b7ba520f12b23674ee81bd1d5fd21fdd1ca7af
unknown <youngscientist.iysa@gmail.com>
2024-03-26 11:01:24 +0700

    open regist

--- src/components/mainhero.jsx
+++ src/components/mainhero.jsx
@@ -4,8 +4,9 @@ function MainHero() {
         <section className="mainhero-section">
             <div className="container">
                 <div className="mx-auto text-center">
-                    <a href="" className="btn btn-action">Segera Hadir 2024</a>
+                    <h1>BUKA PENDAFTARAN 2024</h1>
                     <h1>National Science And Invention Fair</h1>
+                    <a href="https://registrasi-nsif-2024.vercel.app/" target="_blank" className="btn btn-action">DAFTAR DISINI!!</a>
                 </div>
             </div>
         </section>
