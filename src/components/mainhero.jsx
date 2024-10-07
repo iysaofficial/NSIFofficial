793c9cf0181ed82b8c06d76e69248f6e747cc60e
young <youngscientist.iysa@gmail.com>
2024-10-07 10:22:46 +0700

    close

--- src/components/mainhero.jsx
+++ src/components/mainhero.jsx
@@ -6,7 +6,7 @@ function MainHero() {
                 <div className="mx-auto text-center">
                     {/* <h1>BUKA PENDAFTARAN 2024</h1> */}
                     <h1>National Science And Invention Fair</h1>
-                    <a href="https://registrasi-nsif-2024.vercel.app/" target="_blank" className="btn btn-action">DAFTAR DI SINI!!</a>
+                    <a href="/" className="btn btn-action">Pendaftaran Ditutup!!</a>
                 </div>
             </div>
         </section>
