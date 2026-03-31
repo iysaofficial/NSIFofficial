7956a13c9dbb46fd5e89b9b2aa94061f93eae63b
iysaofficial <youngscientist.iysa@gmail.com>
2026-03-31 11:45:38 +0700

    CLOSE 2

--- src/components/mainhero.jsx
+++ src/components/mainhero.jsx
@@ -7,7 +7,7 @@ function MainHero() {
                     {/* <h1>BUKA PENDAFTARAN 2025</h1> */}
                     <h1>National Science And Invention Fair</h1>
                     <button className="btn btn-action m-2" onClick={() => alert("Pendaftaran akan segera dibuka!")}>Pendaftaran Ditutup</button>
-                    <a className="btn btn-action m-2" href="https://drive.google.com/file/d/1Gg2VUAJQHylCc134xzb6qIVq9XEIiRpt/view?usp=sharing" target="_blank">Buku Panduan</a>
+                    {/* <a className="btn btn-action m-2" href="https://drive.google.com/file/d/1Gg2VUAJQHylCc134xzb6qIVq9XEIiRpt/view?usp=sharing" target="_blank">Buku Panduan</a> */}
                 </div>
             </div>
         </section>
